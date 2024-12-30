c677b1d6a6fa89fd10156ebe26a1078462e2535a
Reddy Nithin Reddy <129539615+Nithinreddy000@users.noreply.github.com>
2024-12-30 14:53:21 +0530

    cors2

--- server.js
+++ server.js
@@ -5,24 +5,8 @@ const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-const allowedOrigins = [
-    'https://zp1v56uxy8rdx5ypatb0ockcb9tr6a-oci3--5173--c8c182a3.local-credentialless.webcontainer.io',
-    'http://localhost:5173',
-    // add other origins as needed
-];
-
-// CORS configuration
-app.use(cors({
-    origin: function(origin, callback) {
-        if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
-    methods: ['GET', 'POST'],
-    allowedHeaders: ['Content-Type', 'Authorization']
-}));
+// Simple CORS configuration to allow all origins
+app.use(cors());
 
 // Make sure this comes before your routes
 app.use(express.json());
@@ -41,7 +25,6 @@ app.use('/api/python', require('./api/pythonApi'));
 
 // Java Router
 app.use('/api/java', require('./api/javaApi'))
-
 
 // Serve static files
 app.listen(PORT, () => {
